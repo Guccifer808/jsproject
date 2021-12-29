ba08a8508b637e1de51bdff48bf5e10c103e17e6
Guccifer <hucciallstars@gmail.com>
2021-12-29 18:32:34 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+// alert('Hello');
+
+// const result = confirm("Are you sure?");
+// console.log(result);
+
+// const answer = prompt("Are you okay?", "");
+// console.log(answer);
+
+const answers = [];
+
+answers[0] = prompt('Your name?', '');
+answers[1] = prompt('Your age?', '');
+answers[2] = prompt('Your sex?', '');
+
+console.log(typeof(answers));
